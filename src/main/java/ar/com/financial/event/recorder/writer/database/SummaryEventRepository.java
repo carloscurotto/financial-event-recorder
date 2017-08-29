b29a9e21896e2b1a15e4919b306ed4f24a27e18b
Emanuel Campolo <emacampolo@gmail.com>
2017-08-29 08:46:09 -0300

    Add UI for simple and summary events

--- src/main/java/ar/com/financial/event/recorder/writer/database/SummaryEventRepository.java
+++ src/main/java/ar/com/financial/event/recorder/writer/database/SummaryEventRepository.java
@@ -3,5 +3,9 @@ package ar.com.financial.event.recorder.writer.database;
 import ar.com.financial.event.recorder.domain.SummaryEvent;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface SummaryEventRepository extends CrudRepository<SummaryEvent, String> {
+
+    List<SummaryEvent> findAll();
 }
